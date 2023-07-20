d92bd868ba07088b2c6bf8d4b18393893aecff75
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-20 14:29:05 +0530

    Type promotion in Java

---
+++ JavaBasics/typePromotion.java
@@ -0,0 +1,22 @@
+public class typePromotion {
+    public static void main(String args[]){
+        // byte b = 5;
+        // byte c = (byte)(b*25.2);
+
+        // System.out.println(c);
+
+
+        int $ = 24;
+        System.out.println($);
+
+        byte b = 4; 
+        char c = 'a'; 
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f; 
+        double d = 99.9954;
+        double result = (f * b) + (i % c) - (d * s);
+        System.out.println(result);
+         
+    }
+}
