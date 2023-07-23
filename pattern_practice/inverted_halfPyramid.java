093856bc2694433c5b0ea2a6e1730b562bfcbd5a
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-23 23:22:24 +0530

    Inverted Half Pyramid

---
+++ pattern_practice/inverted_halfPyramid.java
@@ -0,0 +1,22 @@
+public class inverted_halfPyramid {
+
+    public static void invertedHalfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j<=(n-i)){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    
+
+    public static void main(String args[]){
+        
+        invertedHalfPyramid(9);
+    }
+}
