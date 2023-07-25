ccca9a4afbffc319a001fc2adf08f11be787b96f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-25 12:13:39 +0530

    Array in Java

---
+++ JavaBasics/Arr.java
@@ -0,0 +1,16 @@
+public class Arr {
+    public static void main(String args[]){
+        // int arr[] = {2,4,6,8,0};
+        int arr1[] = new int[5];
+        arr1[0] = 2;
+        arr1[1] = 4;
+        arr1[2] = 6;
+        arr1[3] = 8;
+
+
+        for(int i=0;i<arr1.length;i++){
+            System.out.print(arr1[i] + " ");
+        }
+         System.out.println();
+    }
+}
