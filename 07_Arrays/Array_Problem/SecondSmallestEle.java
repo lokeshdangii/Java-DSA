dd41b7785c38c4cb7555a635e9720356e0fcbbe9
lokeshdangii <lokeshdangi1045@gmail.com>
2023-08-09 01:22:42 +0530

    Reverese an Array

--- 07_Arrays/Array_Problem/SecondSmallestEle.java
+++ 07_Arrays/Array_Problem/SecondSmallestEle.java
@@ -1,9 +1,8 @@
+
 // Problem --> To return the index of the second smallest element of the array
 
-import java.util.*;
 
-public class SecondSmallestEle {
-
+public class SecondSmallestEle{
     public static int secondSmallest(int arr[]){
         
         int n = arr.length;
@@ -13,47 +12,43 @@ public class SecondSmallestEle {
         }
 
         if(n==2){
-            return Math.min(arr[0],arr[0+1]);
+            // return Math.min(arr[0],arr[0+1]);
+            if (arr[0]>arr[1]){
+                return 0;
+            }else{
+                return 1;
+            }
         }
 
         // loop to traverse array to find the smallest element of an array
         int res = 0;
-        for(int i=1;i<=n-1; i++ ){
-            if(arr[i]<arr[res]){
-                res = i;
-            }
-        }
         int smallest = arr[res];
-        int second_smallest;
-
-        //  we can take any random element other that the smallest as our second_smallest element to compare with other element
-        // but if the index of the smallest element will be zero than we to took the forward index so there is a IF condition mentioned  
-        if (res == 0){
-             second_smallest = arr[res+1];
-        }else{
-            second_smallest = arr[res-1];
-        }
-       
-        
-        System.out.println("The smallest no is : " + arr[res] );
-        System.out.println("The second smallest no is : " + arr[res-1] );
-        int i;
+        int second_smallest = smallest;
         int index = 0;
-        for (i=0;i<=n-1;i++){
-            
+
+        for(int i=1;i<=n-1; i++ ){
+
+            if(arr[i]<arr[res]){
+                second_smallest = smallest;
+                res = i;
+                smallest = arr[res];
+            }
+
             if( (arr[i] > smallest) && (arr[i] < second_smallest)){
-                second_smallest = arr[i];
-                index = i;   
+                index = i; 
+                second_smallest = arr[i];   
             }
         }
+
         System.out.println("The smallest no is : " + smallest );
         System.out.println("The second smallest no is : " + second_smallest );
         return index;
     }
-    
+
+        
     public static void main(String args[]){
-        // int arr[] = {10,5,8,20,2,6,18,20};
-        int arr[] = {10,9,22};
+        int arr[] = {10,8,20,2,6,5,18,3,20};
+        // int arr[] = {10,9,22};
         System.out.println(secondSmallest(arr));
 
         
