f5a64a2086e9dba269c41aa20d8ab07b9b851105
lokeshdangii <lokeshdangi1045@gmail.com>
2023-08-09 00:24:29 +0530

    return index of second largest element in an array

---
+++ 07_Arrays/Array_Problem/SecondLargestElement.java
@@ -0,0 +1,29 @@
+
+//  Problem --> To return the index of the Second Largest element in an array
+
+public class SecondLargestElement {
+
+    public static int getSecondLargest(int arr[]){
+        int n = arr.length;
+        int second_largest = -1;
+        int largest = 0;
+
+        for (int i=1;i<=n-1;i++){
+            if (arr[i]>arr[largest]){
+                second_largest = largest;
+                largest = i;
+            }else if(arr[i] != arr[largest]){
+                if(second_largest==-1 || arr[i]> arr[second_largest]){
+                    second_largest = i;
+                }
+            }
+        }
+
+        return second_largest;
+    }
+
+    public static void main(String args[]){
+        int arr[] = {5,20,12,20,8};
+        System.out.println("The index of the second largest element is : " + getSecondLargest(arr));
+    }
+}
