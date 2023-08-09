b7caebf47d1ec80fee4c017987038adff9bf1bfa
lokeshdangii <lokeshdangi1045@gmail.com>
2023-08-09 14:17:59 +0530

    Linear Search in Array

---
+++ 07_Arrays/LinearSeach.java
@@ -0,0 +1,25 @@
+public class LinearSeach {
+    
+    public static void linear_Search(int numbers[],int key){
+        
+        for(int i=0;i<numbers.length;i++){
+            if(key==numbers[i]){
+               System.out.println("key element is found at index: " + i );
+            } 
+             
+        }
+             
+            
+
+
+        
+
+    }
+    
+    public static void main(String args[]){
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+     
+           linear_Search(numbers, key);
+    }
+}
