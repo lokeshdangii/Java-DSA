2472ed25f0df1121e7e8a187c58634e2e1e482f4
lokeshdangii <lokeshdangi1045@gmail.com>
2023-09-26 22:05:48 +0530

    Pattern Searching in a text

---
+++ 10_String/PatternSearching.java
@@ -0,0 +1,35 @@
+public class PatternSearching {
+
+    // Function for searching pattern in a text
+    public static void patternSearch(String txt, String pattern){
+        
+        int m = pattern.length();
+        int n = txt.length();
+
+
+        for(int i=0;i<=n-m;i++){
+            int j;
+
+            for(j=0;j<m;j++){
+
+                if(pattern.charAt(j) != txt.charAt(i+j)){
+                    break;
+                }
+            }
+
+             if(j==m){
+                System.out.println(i + " ");
+                }
+            
+        }
+    }
+    
+    // Main Function
+    public static void main(String args[]){
+        String txt = "lokeshdangi";
+        String pattern = "dangi";
+
+        patternSearch(txt, pattern);
+
+    }
+}
