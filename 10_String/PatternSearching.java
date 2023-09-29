4f7352fa46ab0de3514c319431a5a7aae8ead490
lokeshdangii <lokeshdangi1045@gmail.com>
2023-09-29 23:42:01 +0530

    pattern Searching

--- 10_String/PatternSearching.java
+++ 10_String/PatternSearching.java
@@ -26,32 +26,6 @@ public class PatternSearching {
         }
     }
 
-
-    // Improved Naive Solution
-    public static void naiveSolution(String txt, String pattern){
-        
-        int m = pattern.length();
-        int n = txt.length();
-
-
-        for(int i=0;i<=n-m;i++){
-            int j;
-
-            for(j=0;j<m;j++){
-
-                if(pattern.charAt(j) != txt.charAt(i+j)){
-                    break;
-                }
-            }
-
-             if(j==m){
-                System.out.println(i + " ");
-                }
-            
-        }
-    }
-
-
     
     // Main Function
     public static void main(String args[]){
