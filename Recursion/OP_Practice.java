cb5aaead8c9a1785f7e17725a03e15f3cb37d279
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-10 11:59:09 +0530

    Recursion O/P Practice

--- Recursion/OP_Practice.java
+++ Recursion/OP_Practice.java
@@ -24,13 +24,50 @@ public class OP_Practice {
         fun2(n-1);
     }
 
+    // fun3
+    public static int fun3(int n){
 
-    
+        if(n==1){
+            return 0;
+        }
+        else{
+            return 1 + fun3(n/2);
+        }
+    }
+
+    /*
+     * fun(1) = 0
+     * fun(2) = 1 + fun(1) => 1
+     * fun(4) = 1 + fun(2) => 2
+     * fun(8) = 1 + fun(4) => 3
+     * fun(16) = 1 + fun(8) => 4
+     * 
+     * Always OP will be log base 2 n
+    */
+
+    // fun4 
+    public static void fun4(int n){
+        if(n==0){
+            return;
+        }
+       
+        fun4(n/2);
+        System.out.print(n%2 + " ");   // this function gives binary of a n
+    }
+
+
+
 // --------------------------------------------------------- Main --------------------------------------------------------------
     // main function
     public static void main (String args[]){
-        int n = 3;
-        fun1(n);
-        fun2(n);
+        // int n = 3;
+        // fun1(n);
+        // fun2(n);
+
+        int res = fun3(24);
+        System.out.println(res);
+
+        fun4(17);
+    
     }
 }
