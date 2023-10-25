3829f2b142fd638c15b53c5f8e61734be09a05a7
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-25 18:07:13 +0530

    Selection Sort

--- 08_Basic Sorting Algorithms/SelectionSort.java
+++ 08_Basic Sorting Algorithms/SelectionSort.java
@@ -1,28 +1,29 @@
 public class SelectionSort {
     
+    // printArray
+    public static void printArray(int arr[])
+    {           
+           
+        for(int i=0; i<arr.length;i++){
+        System.out.print(arr[i]+ " ");
+    }
+    }
+
+
+    // Selection Sort
     public static void selectionSort(int arr[]){
-
-        for(int i=0;i<arr.length;i++){
-            int min_ele = arr[i];
-
-            for(int j=i+1;j<arr.length;j++){
-                
-                if(arr[j]<min_ele){
-                    min_ele = arr[j];
-
-                    // Swapping
-                    int temp = arr[i];
-                    arr[i] = min_ele;
-                    arr[j] = temp;
-                }
+    for(int i =0; i<arr.length-1;i++){
+        int minPos = i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]< arr[minPos]){
+                minPos = j;
             }
         }
-
-
-        for(int ele : arr){
-            System.out.print(ele + " ");
-        }
-        System.out.println();
+        //swap
+          int temp = arr[minPos];
+          arr[minPos] = arr[i];
+          arr[i] = temp;
+    }
     }
 
     public static void main(String args[]){
@@ -30,23 +31,15 @@ public class SelectionSort {
         int arr[] = {3,6,2,1,8,7,4,5,3,1};
 
         selectionSort(arr);
+        printArray(arr);
     }
-}
+
+    }
+
+    
 
 
-/*
- *public static void selectionSort(int arr[]){
-            for(int i =0; i<arr.length-1;i++){
-                int minPos = i;
-                for(int j=i+1;j<arr.length;j++){
-                    if(arr[minPos]< arr[j]){
-                        minPos = j;
-                    }
-                }
-                //swap
-                  int temp = arr[minPos];
-                  arr[minPos] = arr[i];
-                  arr[i] = temp;
-            }
 
- */
+
+
+
