3b73ac2bdacec21d80a15ebe214da33f19d60748
lokeshdangii <lokeshdangi1045@gmail.com>
2023-12-15 00:07:47 +0530

    Linked HashSet in Java

---
+++ 15_Hashing/H09_LinkedHashSet.java
@@ -0,0 +1,28 @@
+// Linked HashSet --> Order Set same as Linked HashMap
+
+import java.util.LinkedHashSet;
+import java.util.HashSet;
+
+public class H09_LinkedHashSet {
+    
+    public static void main (String args[]){
+
+
+        // HashSet -> Random Order
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(10); hs.add(20); hs.add(30); hs.add(40);
+        System.out.println(hs);
+        
+        // LinkedHashSet -> Prdered Using DLL
+        LinkedHashSet<Integer> Lhs = new LinkedHashSet<>();
+        Lhs.add(90); Lhs.add(20); Lhs.add(33); Lhs.add(40);
+        System.out.println(Lhs);
+   
+        // Iterating by for each Loop
+        for(Integer k : Lhs)
+            System.out.println(k);
+
+        for(Integer k : hs)
+            System.out.println(k);
+    }
+}
