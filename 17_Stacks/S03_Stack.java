9fc5899da6be5337be2156e523dae4389e82bdc6
lokeshdangii <lokeshdangi1045@gmail.com>
2024-01-13 22:20:27 +0530

    Stack using JCFC

---
+++ 17_Stacks/S03_Stack.java
@@ -0,0 +1,16 @@
+// Stack by Java Collections Framework
+import java.util.*;
+
+public class S03_Stack {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
