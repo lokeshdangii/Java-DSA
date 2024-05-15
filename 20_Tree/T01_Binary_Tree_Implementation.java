03d306d0e9975f1a6b305220dc8b241b6bd11418
lokeshdangii <lokeshdangi1045@gmail.com>
2024-05-15 22:55:05 +0530

    Inorder Traversal Implementation

--- 20_Tree/T01_Binary_Tree_Implementation.java
+++ 20_Tree/T01_Binary_Tree_Implementation.java
@@ -1,4 +1,4 @@
-public class Binary_Tree_Implementation {
+public class T01_Binary_Tree_Implementation {
     static class Node{
         int data;
         Node left;
@@ -10,12 +10,23 @@ public class Binary_Tree_Implementation {
             right = null;
         } 
     }    
-    
+
+    // Inorder Traversal --> TC = O(n)  & SC = O(h) h --> height of tree
+    public static void inorder(Node root){
+        if(root != null){
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
     public static void main(String[] args) {
         Node root = new Node(10);
         root.left = new Node(20);
         root.right = new Node(30);
         root.left.left = new Node(40);
+
+        inorder(root);
     }
 
 }
