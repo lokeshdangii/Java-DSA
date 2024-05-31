eea0bc02d3bfeb7121266c197d0eb0322b738ba9
lokeshdangii <lokeshdangi1045@gmail.com>
2024-05-31 17:52:00 +0530

    Red Black Tree

---
+++ 21_BST/B04_RBT.java
@@ -0,0 +1,14 @@
+/* Red Black Tree
+ * 1) Every node is either Red or Black
+ * 2) Root is always black
+ * 3) No two consecutive Reds
+ * 4) Number of black nodes from node to all of its descandents should be same.
+ 
+ * RBT is loose as compare to AVL Tree
+ * In RBL trees insert and delter operations are complex and seach is same as the BST.
+ * In insert and delete operations we do some restructuring if insertion or deletion causes unbalancing.
+ */
+
+public class B04_RBT {
+    
+}
