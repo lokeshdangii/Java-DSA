5fb9091f4616ecf6e06969adb80dabee51bdc62e
lokeshdangii <lokeshdangi1045@gmail.com>
2024-06-22 13:06:36 +0530

    Greedy Approach

---
+++ 25_Greedy/G01_Greedy.java
@@ -0,0 +1,10 @@
+/*
+ Greedy Algorithm is the problem solving technique where we make the locally optimum (Best Solution) choice at each stage & hope to achieve a global optimum.
+ 
+ Pros:- 
+    --> Simple & Easy
+    --> Good enough TC
+
+ Cons:-
+    --> A lot of time global optimum is not achieved
+ */
